031cff194850b244a6ddfc81d08b8778dadf2586
Heshith619 <it19134390@my.sliit.lk>
2023-07-10 10:59:03 +0530

    Add project files.

---
+++ BulkyBook/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; } //ID is primary key
+
+    [Required] //NAME IS REQUIRED PROPERTY
+    public string Name{ get; set; }
+    public string DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; }= DateTime.Now;
+
+ }
